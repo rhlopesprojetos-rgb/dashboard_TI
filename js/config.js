efc3243712fff4e7ab630d2771790dbeca2b495a
rhlopesprojetos-rgb <rhlopesprojetos@gmail.com>
2026-07-27 17:22:19 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Cole aqui a URL /exec gerada ao implantar o Code.gs como Web App.
-const APPS_SCRIPT_URL = 'COLE_AQUI_A_URL_DO_APPS_SCRIPT';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwNHpy3Jpeg6Kl7ZCkqIiHgW42mm0hscKjYy-P1h8m5qEpY_e-CSSo2JEIrJlnJO7r2Gg/exec';
